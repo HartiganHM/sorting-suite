038fa2ad009fcd7b72e3f7c8e6e04fba7d1c70d0
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-18 06:30:43 -0600

    Removed console.logs

--- scripts/insertionSort.js
+++ scripts/insertionSort.js
@@ -1,13 +1,11 @@
 const insertionSort = array => {
   for (let i = 1; i < array.length; i++) {
     for (let j = i - 1; j >= 0; j--) {
-      console.log(array)
       if (array[j] > array[j + 1]) {
         [array[j + 1], array[j]] = [array[j], array[j + 1]];
       }
     }
   }
-  console.log(array);
   return array;
 };
 
