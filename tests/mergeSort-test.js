75722acf3099ff13cfe53c02d06c7eea61ddb8b0
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-18 13:04:45 -0600

    Built tests up to 100,000 values, breaks at 150,000

--- tests/mergeSort-test.js
+++ tests/mergeSort-test.js
@@ -3,69 +3,84 @@ const mergeSort = require('../scripts/mergeSort.js');
 const randomArray = require('../scripts/randomNumber.js').randomArray;
 const randomArrayCheck = require('../scripts/randomNumber.js').randomArrayCheck;
 
-describe('Insertion Sort Tests', function() {
+describe('Merge Sort Tests', function() {
 
 	it('Should be a function', function() {
 		assert.isFunction(mergeSort);
 	});
 
-	it.skip('Should sort a small array', function() {
+	it('Should sort a small array', function() {
 		let array = [ 7, 3 ];
-		mergeSort(array);
-		assert.deepEqual(array, [ 3, 7 ]);
+		assert.deepEqual(mergeSort(array), [ 3, 7 ]);
 	});
 
-	it.skip('Should sort an array of ten numbers', function() {
+	it('Should sort an array of ten numbers', function() {
 		let array = [ 3, 7, 2, 1, 0, 6, 42, 58, 29, 11 ];
-		mergeSort(array);
-		assert.deepEqual(array, [ 0, 1, 2, 3, 6, 7, 11, 29, 42, 58 ]);
+		assert.deepEqual(mergeSort(array), [ 0, 1, 2, 3, 6, 7, 11, 29, 42, 58 ]);
 	});
 
-	it.skip('Should be able to sort larger numbers', function() {
+	it('Should be able to sort larger numbers', function() {
 		let array = [ 1000, 2381, 10923, 13891, 532, 23535 ];
-		mergeSort(array);
-		assert.deepEqual(array, [ 532, 1000, 2381, 10923, 13891, 23535 ]);
+		assert.deepEqual(mergeSort(array), [ 532, 1000, 2381, 10923, 13891, 23535 ]);
 	});
 
-	it.skip('Should be able to sort negative numbers', function() {
+	it('Should be able to sort negative numbers', function() {
 		let array = [ 1, 5, -17, 9, 10, -4, -1, 0 ];
-		mergeSort(array);
-		assert.deepEqual(array, [ -17, -4, -1, 0, 1, 5, 9, 10 ]);
+		assert.deepEqual(mergeSort(array), [ -17, -4, -1, 0, 1, 5, 9, 10 ]);
 	});
 
-	it.skip('Should be able to sort letters', function() {
+	it('Should be able to sort letters', function() {
 		let array = [ 'v', 't', 'j', 'c' ];
-		mergeSort(array);
-		assert.deepEqual(array, [ 'c', 'j', 't', 'v' ]);
+		assert.deepEqual(mergeSort(array), [ 'c', 'j', 't', 'v' ]);
 	});
 
-	it.skip('Should be able to sort a random array of 10 numbers', function() {
+	it('Should be able to sort a random array of 10 numbers', function() {
 		let array = randomArray(1, 20, 10);
 		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
 	});
 
-	it.skip('Should be able to sort a random array of 100 numbers', function() {
+	it('Should be able to sort a random array of 100 numbers', function() {
 		let array = randomArray(1, 200, 100);
 		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
 	});
 
-	it.skip('Should be able to sort a random array of 1,000 numbers', function() {
+	it('Should be able to sort a random array of 1,000 numbers', function() {
 		let array = randomArray(1, 2000, 1000);
 		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
 	});
 
-	it.skip('Should be able to sort a random array of 10,000 numbers', function() {
+	it('Should be able to sort a random array of 10,000 numbers', function() {
 		let array = randomArray(1, 20000, 10000);
 		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
 	});
 
-	it.skip('Should be able to sort a random array of 20,000 numbers', function() {
+	it('Should be able to sort a random array of 20,000 numbers', function() {
 		let array = randomArray(1, 40000, 20000);
 		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
 	});
 
-	it.skip('Should be able to sort a random array of 35,000 numbers', function() {
+	it('Should be able to sort a random array of 35,000 numbers', function() {
 		let array = randomArray(1, 70000, 35000);
+		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
+	});
+
+	it('Should be able to sort a random array of 50,000 numbers', function() {
+		let array = randomArray(1, 100000, 50000);
+		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
+	});
+
+	it('Should be able to sort a random array of 75,000 numbers', function() {
+		let array = randomArray(1, 150000, 75000);
+		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
+	});
+
+	it('Should be able to sort a random array of 100,000 numbers', function() {
+		let array = randomArray(1, 200000, 100000);
+		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
+	});
+
+	it.skip('Should be able to sort a random array of 150,000 numbers', function() {
+		let array = randomArray(1, 300000, 150000);
 		assert.deepEqual(randomArrayCheck(mergeSort(array)), true);
 	});
 })
