5d949991df257fa62efe8bad31cec79024fde0b5
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-17 21:13:25 -0600

    Fixed incorrect parameter label and added a test function to check if random array is sorted

--- scripts/randomNumber.js
+++ scripts/randomNumber.js
@@ -1,10 +1,20 @@
 function randomArray (lowerLimit, upperLimit, arraySize) {
   var resultArray = [];
-  for (var i = 0 ; i < arraySizeValue; i++){
+  for (var i = 0 ; i < arraySize; i++){
     var random = Math.floor(Math.random() * ( upperLimit - lowerLimit ) + lowerLimit);
     resultArray.push(random);
   }
   return resultArray;
+}
+
+function randomArrayTest (sortedArray) {
+	for (let i = 0; i < sortedArray.length; i++) {
+		if (sortedArray[i] <= sortedArray[i + 1]) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 
 module.exports = randomArray;
