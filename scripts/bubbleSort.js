90b664825e59dc83441bc511374ca968933bd3bb
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-10-19 15:11:23 -0600

    Optimized bubble and insertion sort from lesson

--- scripts/bubbleSort.js
+++ scripts/bubbleSort.js
@@ -1,15 +1,15 @@
 const bubbleSort = array => {
-  for (let i = 0; i < array.length - 1; i++) {
-    let didNotSwap = true;
+  for (let i = 0; i < array.length; i++) {
+    let didSwap = false;
 
-    for (let j = 0; j < array.length - 1; j++) {
+    for (let j = 0; j < array.length - i; j++) {
       if (array[j] > array[j + 1]) {
+        didSwap = true;
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
-        didNotSwap = false;
       }
     }
 
-    if (didNotSwap === true) {
+    if (!didSwap) {
       return array;
     }
   }
--- scripts/insertionSort.js
+++ scripts/insertionSort.js
@@ -1,9 +1,7 @@
 const insertionSort = array => {
   for (let i = 1; i < array.length; i++) {
-    for (let j = i - 1; j >= 0; j--) {
-      if (array[j] > array[j + 1]) {
-        [array[j + 1], array[j]] = [array[j], array[j + 1]];
-      }
+    for (let j = i - 1; j >= 0 && array[j] > array[j + 1]; j--) {
+    	[array[j], array[j + 1]] = [array[j + 1], array[j]];
     }
   }
   
